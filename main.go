d133591cbbb488a860f197495e42f89a7084536b
marc.sumner <marc.sumner@averbis.com>
2024-02-04 18:34:13 +0100

    db client with redis

--- main.go
+++ main.go
@@ -4,12 +4,23 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/robbiesumner/go-url/db_store"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, World")
-    })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World")
+	})
 
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	dbClient := db_store.NewDBClient()
+	err := dbClient.Set("foo1", "bar1")
+	fmt.Println(err)
+	err = dbClient.Set("foo1", "bar2")
+	fmt.Println(err)
+	val, err := dbClient.Get("foo2")
+	fmt.Println(err, val)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
